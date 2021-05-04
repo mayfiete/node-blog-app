afe676a4360c42c31f09cb3dae751fff4e427db4
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-05-03 21:23:22 -0400

    feat: adding html pages and updated index.js file

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const http = require('http')
-const server = http.createServer((req, res) => {
-    console.log(req.url) 
-    res.end('Hello Node.js')
+const fs = require('fs') 
+const homePage = fs.readFileSync('index.html') 
+const aboutPage = fs.readFileSync('about.html') 
+const contactPage = fs.readFileSync('contact.html') 
+const notFoundPage = fs.readFileSync('notFound.html')
+
+const server = http.createServer((req, res) => { 
+    if(req.url=== '/about')
+        res.end(aboutPage)
+    else if (req.url=== '/contact')
+        res.end(contactPage) 
+    else if (req.url ==='/') 
+        res.end(homePage)
+    else {
+        res.writeHead(404) 
+        res.end(notFoundPage)
+    }
 })
 
 server.listen(3000)
